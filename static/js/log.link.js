e63aae851e5c61403ac5b9a4693da1e8773b5b70
kpmy <petryxa.clever@gmail.com>
2016-01-08 02:00:19 +0300

    фикс ссылки на логи

--- static/js/log.link.js
+++ static/js/log.link.js
@@ -3,7 +3,8 @@
  */
 $(function(){
     var d = new Date();
-	var month = d.getMonth < 9 ? "0" + (d.getMonth() + 1) : d.getMonth() + 1;
+	var month = d.getMonth() < 9 ? "0" + (d.getMonth() + 1) : d.getMonth() + 1;
     var day = d.getDate() <= 9 ? "0" + d.getDate() : d.getDate();
+    console.log(month);
     $("#today").attr("href", "http://chatlogs.jabber.ru/golang@conference.jabber.ru/"+d.getFullYear()+"/"+month+"/"+day+".html");
 });
